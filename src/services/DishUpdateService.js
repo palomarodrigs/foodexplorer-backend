ff969136618b5a19e0b9e2e6f42f52d327a8a43c
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2023-04-26 19:14:12 -0300

    Feat: added new condition compare title with current before update

--- src/services/DishUpdateService.js
+++ src/services/DishUpdateService.js
@@ -12,6 +12,10 @@ class DishUpdateService {
       throw new AppError('Dish not found!')
     }
 
+    if (title === dish.title) {
+      throw new AppError('New title must be different from current title.')
+    }
+
     dish.title = title ?? dish.title
     dish.description = description ?? dish.description
     dish.price = price ?? dish.price
